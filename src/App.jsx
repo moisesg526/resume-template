6373515bea9ef49c2fba522495c2b7dec8b47b5f
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-12-09 00:00:45 -0500

    Added the experience and education component to app file and added a div to divide from the resume section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./styles/app.css";
 import Resume from "./component/Resume";
 import GeneralInfo from "./component/GeneralInfo";
+import Experience from "./component/Experience";
+import Education from "./component/Education";
 
 function App() {
   return (
     <div className="app">
-      <GeneralInfo />
+      <div className="input-section">
+        <GeneralInfo />
+        <Experience />
+        <Education />
+      </div>
       <Resume />
     </div>
   );
